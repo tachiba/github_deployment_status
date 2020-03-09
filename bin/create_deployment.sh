cc1f380651821ace3680c272caf9e56baec1f4ec
Takashi Chiba <work@tachiba.jp>
2020-03-09 20:47:57 +0900

    ✨ Ignore checks

--- bin/create_deployment.sh
+++ bin/create_deployment.sh
@@ -6,7 +6,7 @@ REF=$2
 DEPLOYMENT=$(curl \
   -s \
   -H "Authorization: token ${GITHUB_TOKEN}" \
-  -d "{ \"ref\": \"${REF}\", \"auto_merge\": false }" \
+  -d "{ \"ref\": \"${REF}\", \"auto_merge\": false, \"required_context\": [] }" \
   https://api.github.com/repos/${REPO}/deployments)
 
 DEPLOYMENT_ID=$(echo "${DEPLOYMENT}" | jq -r ".id")
